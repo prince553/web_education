1f5f7bb7b78cdda9857bb02f55c697e0c5bfd399
prince <prince553@gmail.com>
2023-06-26 14:43:44 +0530

    first-connit

---
+++ assets/environment/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    url: 'https://education-app-backend.vercel.app/api/',
+    // urlTest : 'http://192.168.0.164:4001/api/standards',
+    // urlTestSubject : 'http://192.168.0.164:4001/api/subjects',
+    // studentChangePasswordUrl : 'http://192.168.0.164:4001/api/students/change-password',
+    // teacherChangePasswordUrl : 'http://192.168.0.164:4001/api/teachers/change-password',
+    // studentReferFriendsLinkUrl : 'http://192.168.0.164:4001/api/referFriends',
+    // teacherReferFriendsLinkUrl : 'http://192.168.0.164:4001/api/referFriends',
+    // teacherGetPaper : 'http://192.168.0.164:4001/api/questionPapers',
+    // studnetSearchPaper : 'http://192.168.0.164:4001/api/searchPaper'
+    };
+  
